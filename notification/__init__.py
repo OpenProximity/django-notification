5ee8ff05c37ccce16083a92d886a80217e6fba85
Manuel Francisco Naranjo <manuel@aircable.net>
2011-09-28 00:24:12 -0700

    mark version number with our stuff, make self contained so that distutils can do it's job

--- notification/__init__.py
+++ notification/__init__.py
@@ -1,4 +1,4 @@
-VERSION = (0, 1, 5, "final")
+VERSION = (0, 1, 5, "-final-openproximity")
 
 def get_version():
     if VERSION[3] != "final":
@@ -8,17 +8,32 @@ def get_version():
 
 __version__ = get_version()
 
-from django.conf import settings
-
-# default configuration
-NOTIFICATION_DEFAULT_SITE_NAME = getattr(settings, 'NOTIFICATION_DEFAULT_SITE_NAME', 'Django Notifications')
-NOTIFICATION_USE_SITE = getattr(settings, 'NOTIFICATION_USE_SITE', False)
-NOTIFICATION_QUEUE_ALL = getattr(settings, "NOTIFICATION_QUEUE_ALL", False)
-NOTIFICATION_LANGUAGE_MODULE = getattr(settings, 'NOTIFICATION_LANGUAGE_MODULE', False)
-NOTIFICATION_FEEDS = getattr(settings, "NOTIFICATION_FEEDS", False)
-NOTIFICATION_LOCK_WAIT_TIMEOUT = getattr(settings, "NOTIFICATION_LOCK_WAIT_TIMEOUT", -1)
-NOTIFICATION_ITEMS_PER_FEED = getattr(settings, 'NOTIFICATION_ITEMS_PER_FEED', 20)
-DEFAULT_HTTP_PROTOCOL = getattr(settings, "DEFAULT_HTTP_PROTOCOL", "http")
-
-
-
+try:
+    from django.conf import settings
+	# default configuration
+    NOTIFICATION_DEFAULT_SITE_NAME = getattr(settings, 
+                                             'NOTIFICATION_DEFAULT_SITE_NAME', 
+                                             'Django Notifications')
+    NOTIFICATION_USE_SITE = getattr(settings, 
+                                    'NOTIFICATION_USE_SITE',
+                                    False)
+    NOTIFICATION_QUEUE_ALL = getattr(settings, 
+                                     "NOTIFICATION_QUEUE_ALL", 
+                                     False)
+    NOTIFICATION_LANGUAGE_MODULE = getattr(settings, 
+                                           'NOTIFICATION_LANGUAGE_MODULE', 
+                                           False)
+    NOTIFICATION_FEEDS = getattr(settings, 
+                                 "NOTIFICATION_FEEDS", 
+                                 False)
+    NOTIFICATION_LOCK_WAIT_TIMEOUT = getattr(settings, 
+                                             "NOTIFICATION_LOCK_WAIT_TIMEOUT", 
+                                             -1)
+    NOTIFICATION_ITEMS_PER_FEED = getattr(settings, 
+                                          'NOTIFICATION_ITEMS_PER_FEED',
+                                          20)
+    DEFAULT_HTTP_PROTOCOL = getattr(settings, 
+                                    "DEFAULT_HTTP_PROTOCOL", 
+                                    "http")
+except:
+	pass
